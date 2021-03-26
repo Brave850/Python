c40dc8c666f2e9c478c1ce4488e38013fdb4148b
Sambit Khandai <67492957+Brave850@users.noreply.github.com>
2021-03-26 11:09:01 +0530

    Update count_No_of_Character.py

--- Projects/count_No_of_Character.py
+++ Projects/count_No_of_Character.py
@@ -1,12 +1,14 @@
 def count(st):
- all_freq = {}
+ freq={}
  for i in st:
-  if i in all_freq:
-   all_freq[i] += 1
+  if i in freq:
+   freq[i] += 1
   else:
-   all_freq[i] = 1
- for x in all_freq:
-  print(x," = ",all_freq[x])        	
+   freq[i] = 1
+ for x in freq:
+  print(x," = ",freq[x]) 
+
+
 
 st=input("Enter the string: ")
 count(st)
